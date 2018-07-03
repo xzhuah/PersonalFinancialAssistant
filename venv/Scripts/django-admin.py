1ad951bcd9f9b47369db7cad6e69e97ce3f2cc02
ZHU Xinyu <xzhuah@connect.ust.hk>
2018-07-04 00:45:30 +0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\CMULearn\Study\2018 Summer\advanced python\PersonalFinancialAssistant\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
